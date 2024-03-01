1d1dad446d391d2a26dab3e9c1ea24304b017460
shahid.abbas@rootpointers.info <shahid.abbas@rootpointers.info>
2024-03-01 15:51:04 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+using CronJob_DotNet6.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddCronJob<CronJob1>(c =>
+{
+    c.TimeZoneInfo = TimeZoneInfo.Local;
+    c.CronExpression = "0 0 * * *"; // 0th minute, 0th hour (12:00 AM)
+});
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ Services/CronJob1.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace CronJob_DotNet6.Services
+{
+    public class CronJob1 (IScheduleConfig<CronJob1> config)
+        : CronJobService(config.CronExpression, config.TimeZoneInfo)
+    {
+        public override Task StartAsync(CancellationToken cancellationToken)
+        {
+            Console.WriteLine("CronJob 1 starts.");
+            return base.StartAsync(cancellationToken);
+        }
+
+        public override Task DoWork(CancellationToken cancellationToken)
+        {
+            Console.WriteLine($"CronJob 1 is working.{DateTime.Now}");
+            return Task.CompletedTask;
+        }
+
+        public override Task StopAsync(CancellationToken cancellationToken)
+        {
+            Console.WriteLine("CronJob 1 is stopping.");
+            return base.StopAsync(cancellationToken);
+        }
+    }
+}
